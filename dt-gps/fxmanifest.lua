3f6fbc6bc293e258a56ae609ac6b0b69713ab5fa
DinoCROTH <58315706+DinoCROTH@users.noreply.github.com>
2025-06-12 18:51:04 +0200

    Add files via upload

---
+++ dt-gps/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'DinoCROTH'
+name 'dt-gps'
+description 'QBCore GPS Tracker'
+version '1.0.0'
+
+client_script 'client.lua'
+server_script 'server.lua'
+shared_script 'config.lua'
